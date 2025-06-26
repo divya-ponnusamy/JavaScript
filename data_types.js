7e87433b21eab1f804fddbecee596f2ebac865be
divya-ponnusamy <pdivyajob@gmail.com>
2025-06-26 15:32:26 +0530

    add data_types

---
+++ data_types.js
@@ -0,0 +1,55 @@
+//data type
+let name_n="divya";
+let ages=25;
+let isstudent=true;
+console.log(name_n,ages=55,isstudent);
+
+const namee="divya";
+const agee=45;
+const isstudentt=false;
+console.log(namee,agee,isstudentt)
+
+//string
+let firstname="divya";
+let lastname="ponnusamy";
+console.log("fullname",firstname+" "+lastname);
+
+//number
+let age =45;
+let height=142.5;
+sum=age+height;
+console.log("sum of age and height",sum);
+
+//boolean
+let isStudent=true;
+let hasgrauted=false;
+console.log("istudent",isStudent);
+console.log("hasgrauate",hasgrauted);
+
+//undefiend
+let job;
+console.log("job",job);
+
+//null
+let salary=null;
+console.log("salary",salary);
+
+//object
+let persons={
+    name:"divya",
+    age:24,
+    isStudent:true
+};
+console.log("persons details",persons);
+
+//array
+let fruits=["apple","mango","graps"];
+console.log("first fruit",fruits[0]);
+console.log("all fruits",fruits)
+
+//typeof operator
+
+let a=22;
+let names="divya";
+console.log(typeof(a));
+console.log(typeof(names))
